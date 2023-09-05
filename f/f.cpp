11fdba5953920f8625bab1f1ac5f239064607ef5
7mantel7 <aper29012006@gmail.com>
2023-09-05 20:47:33 +0300

    Кости

--- f/f.cpp
+++ f/f.cpp
@@ -1,10 +1,24 @@
-﻿
+﻿#include <time.h>
 #include <iostream>
+#include <random>
 using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    return 0;
+    setlocale(LC_ALL, "");
+    srand(time(0));
+    bool flag = true;
+    while (flag) {
+        string answer;
+        cout << "Если хотите бросить кости введите любую цифру кроме 0, для выхода введите 0: " << endl;
+        cin >> answer;
+        if (answer == "0") {
+            flag = false;
+        }
+        else {
+            int a = rand() % 6 + 1;
+            cout << "Ваша цифра: " << a << endl;
+        }
+    }
 }
 
